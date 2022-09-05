12a0b83846db40c094dedb66b6ae4bb000069277
Kalle-Muenster <kalle.muenster@outlook.de>
2022-09-05 07:58:36 +0200

    update pack!

--- Pack.bat
+++ Pack.bat
@@ -1,11 +1,11 @@
 @if not "ECHO_STATE"=="" (@echo %ECHO_STATE%) else (@echo off)
 set _here_=%CD%
 cd /d %~dp0
-cd YpsCrypt
+cd YpsCrypt50
 dotnet restore
 cd..
 cd YpsTests
-dotnet restore
+dotnet restore YpsTests50.csproj
 cd /d %_here_%
 set _here_=
 
