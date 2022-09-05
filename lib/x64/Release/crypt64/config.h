50b19d8943dc97ecf70eabf021b25e5e49b8e459
Kalle Muenster <kalle@froglogic.com>
2022-09-05 07:25:06 +0200

    Add missed file config.h

---
+++ lib/x64/Release/crypt64/config.h
@@ -0,0 +1,16 @@
+#ifndef CRYPT64_CONFIGURATION
+#define CRYPT64_CONFIGURATION
+
+#define USE_COMMANDLINER (1)
+#define USE_JUNKYARD (1)
+#define USE_STRINGPOOL (0)
+#define USE_HEXSTRING (0)
+#define USE_FOURCC (1)
+#define USE_BASE64 (1)
+#define USE_CRYPT64 (1)
+#define USE_TOKKEN (0)
+
+#define SET_MAX_NAMELENGTH 512
+#define SET_MAX_NUMBERARGS 8
+
+#endif
